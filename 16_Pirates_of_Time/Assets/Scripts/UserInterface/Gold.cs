4067e6c667ea5e634bfe7b2ae920ea8736a2b9a3
JakeHildy <jake@hildy.ca>
2020-05-03 11:15:46 -0700

    Added Gold UI

---
+++ 16_Pirates_of_Time/Assets/Scripts/UserInterface/Gold.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Gold : MonoBehaviour
+{
+
+    [SerializeField] int startingGold = 100;
+    [SerializeField] TextMeshProUGUI goldText;
+
+    private int gold;
+
+    void Start()
+    {
+        goldText.text = startingGold.ToString();
+        gold = startingGold;
+    }
+
+
+}
