27de9abd0d17ec17da32e85848d243228d3c7828
JakeHildy <jake@hildy.ca>
2020-05-10 14:10:14 -0700

    Added Cannonballs to UI

--- 16_Pirates_of_Time/Assets/Scripts/Combat/Cannon.cs
+++ 16_Pirates_of_Time/Assets/Scripts/Combat/Cannon.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using RPG.UserInterface;
 
 namespace RPG.Combat
 {
@@ -9,17 +10,29 @@ namespace RPG.Combat
 
         [SerializeField] GameObject barrelLocation;
         [SerializeField] GameObject cannonBallPrefab;
+        [SerializeField] bool aIControlled = false;
+
+        Cannonballs cannonballs;
 
         public float shotPower = 100f;
 
 
+        private void Start()
+        {
+            cannonballs = FindObjectOfType<Cannonballs>();
+        }
+
         public void Shoot()
         {
-            GameObject cannonBall;
-            cannonBall = Instantiate(cannonBallPrefab, barrelLocation.transform.position, barrelLocation.transform.rotation) as GameObject;
-            cannonBall.GetComponent<Rigidbody>().AddForce(barrelLocation.transform.forward * shotPower);
-            Destroy(cannonBall, 5f);
-            GetComponent<AudioSource>().Play();
+            if (aIControlled || cannonballs.TryToUseCannonBall())
+            {
+                GameObject cannonBall;
+                cannonBall = Instantiate(cannonBallPrefab, barrelLocation.transform.position, barrelLocation.transform.rotation) as GameObject;
+                cannonBall.GetComponent<Rigidbody>().AddForce(barrelLocation.transform.forward * shotPower);
+                Destroy(cannonBall, 5f);
+                GetComponent<AudioSource>().Play();
+            }
+
         }
     }
 }
---
+++ 16_Pirates_of_Time/Assets/Scripts/UserInterface/Cannonballs.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace RPG.UserInterface
+{
+    public class Cannonballs : MonoBehaviour
+    {
+        [SerializeField] int startingCannonballs = 50;
+        [SerializeField] TextMeshProUGUI cannonballText;
+
+        private int cannonballs;
+
+        void Start()
+        {
+            cannonballs = startingCannonballs;
+            UpdateCannonballText();
+        }
+
+
+        public void AddCannonBalls(int amount)
+        {
+            cannonballs += amount;
+            UpdateCannonballText();
+        }
+
+        public bool TryToUseCannonBall()
+        {
+            if (cannonballs > 0)
+            {
+                cannonballs--;
+                UpdateCannonballText();
+                return true;
+            }
+            else
+            {
+                Debug.Log("Out of Cannonballs!");
+                return false;
+            }
+
+        }
+
+        private void UpdateCannonballText()
+        {
+            cannonballText.text = cannonballs.ToString();
+        }
+
+    }
+}
+
